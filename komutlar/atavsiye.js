474545b44686efee57c121ac3b17161946bce7e3
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-05-29 19:59:44 +0300

    Add files via upload

---
+++ komutlar/atavsiye.js
@@ -0,0 +1,29 @@
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(' ')[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(' ').slice(1);
+
+  if (command === 'tavsiyeni-gönder' || command === 'tavsiye') {
+    let str = '<@419936204117770241>';//@silmeyin!
+    let id = str.replace(/[<@!>]/g, '');
+    let mesaj = args.slice(0).join(' ');
+    if (mesaj.length < 1) return message.reply(` ⚠ Tavsiyeni yazmayı unuttun. ⚠ `);
+    message.channel.sendEmbed(new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setDescription(''));
+    const embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setTitle('Tavsiye bilgileri;')
+    .addField('Tavsiye:', mesaj, true)
+    .addField('Kullanıcı adı:', message.author.tag, true)
+    .addField('Kullanıcı kimliği:', message.author.id, true)
+    .addField('Sunucu adı:', message.guild.name, true)
+    .addField('Sunucu kimliği:', message.guild.id, true)
+    client.fetchUser(id)
+    .then(user => {user.send({embed})})
+  }
+});
