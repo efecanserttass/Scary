8b74f1c5636637aca191ffdd85eb7c6ff85f518c
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-05-29 03:35:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,7 +93,7 @@ client.unload = command => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('**Aleyküm Selam, Hoşgeldin Scary Davet Etmek İçin ```s!davet``` Yazabilirsiniz**');
+    msg.reply('**Aleyküm Selam, Hoşgeldin**');
   }
 });
 
