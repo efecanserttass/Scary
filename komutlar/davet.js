d4a0ce5d82875c00a559347726523ff7e9d24339
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-05-29 21:46:29 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -4,20 +4,20 @@ const ayarlar = require('../ayarlar.json');
 
 exports.run = (client, message) => {
   const embed = new Discord.RichEmbed()
-  .setTitle("Tıkla ve Fcary'i Davet Et !")
-  .setAuthor("Fcary", "")
+  .setTitle("Tıkla ve Davet Et !")
+  .setAuthor("Scary", "")
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0xff8080)
-  .setDescription(" Fcary'i Çok Seviceksiniz Botumuz Yeni Yapılmıştır Eklerseniz Seviniriz.")
-  .setFooter("©️ 2018 Fcary | Yusuf | Murat ® ", "")
+  .setDescription(" Scary'i Çok Seviceksiniz Botumuz Yeni Yapılmıştır Eklerseniz Seviniriz. (╯°□°）╯")
+  .setFooter("©️ 2018 Scary| Yusuf |® ", "")
   .setThumbnail("")
   /*
    * Takes a Date object, defaults to current date.
    */
   .setTimestamp()
-  .setURL('https://discordapp.com/oauth2/authorize?client_id=450551931950333962&scope=bot&permissions=805665854')
+  .setURL('https://discordapp.com/oauth2/authorize?client_id=449814712025088010&permissions=8&scope=bot')
   
   message.channel.send({embed});
 };
