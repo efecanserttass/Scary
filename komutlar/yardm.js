993bc9ee9d7df1516b394a0922cd939757b89fb1
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-05-29 16:20:37 +0300

    Update yardm.js

--- komutlar/yardm.js
+++ komutlar/yardm.js
@@ -7,12 +7,11 @@ module.exports.run = async (bot, message, args) => {
     .setAuthor("85 Commands", bot.user.displayAvatarURL)
     .addField("Moderation", "`" + message.prefix + "mod`", true)
     .addField("Games", "`" + message.prefix + "games`", true)
-
-
+    .addField("Anime", "`" + message.prefix + "animecmds`", true)
+    .addField("Music", "`" + message.prefix + "music`", true)
     .addField("NSFW", "`" + message.prefix + "nsfw`", true)
-  
-
-
+    .addField("Core", "`" + message.prefix + "core`", true)
+    .addField("Utils", "`" + message.prefix + "utils`", true)
     .addField("Fun", "`" + message.prefix + "fun`", true)
     .addField("Statistics", "`" + message.prefix + "statistics`", true)
     .addField("Developer", "`" + message.prefix + "dev`", true)
