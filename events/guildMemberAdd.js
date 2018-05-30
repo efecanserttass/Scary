a6f30d7346f9d033d494fa334ae5a12a5e4dce62
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-05-30 08:58:37 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -14,14 +14,14 @@ client.on('guildMemberAdd', member => {
   channel.sendEmbed(embed);
 });
 
-client.on('guildMemberRemove', member => {
-  const channel = member.guild.channels.find('name', 'scary');
-  if (!channel) return;
-  const embed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-  .setAuthor(member.user.username, member.user.avatarURL)
-  .setThumbnail(member.user.avatarURL)
-  .setTitle('📤 | Bir Kaybımız Var Yastayız 😔| Görüşmek üzere!')
-  .setTimestamp()
-  channel.sendEmbed(embed); 
-});
+
+ 
+ 
+  
+  
+  
+  
+
+
+ 
+
