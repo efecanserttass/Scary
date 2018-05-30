202a7c605033c7a0139609aae52fe275442e6ad8
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-05-30 09:01:17 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,13 @@
-module.exports = member => {
-  let guild = member.guild;
-  member.send('niye gittin ???');
-  guild.defaultChannel.send(`${member.user.username} gitti.`);
-};
+client.on('guildMemberRemove', member => {
+  const channel = member.guild.channels.find('name', 'scary');
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setAuthor(member.user.username, member.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle('📤 | Bir Kaybımız Var Yastayız 😔| Görüşmek üzere!')
+  .setTimestamp()
+  channel.sendEmbed(embed); 
+});
+
+
