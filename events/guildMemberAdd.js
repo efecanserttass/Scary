b035bd1c15587f90e1523383760974737a3b88bc
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-06-02 02:57:15 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,11 @@
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('Hoş geldin **' + username + '**! Buraya arkadaşlarını davet edebilir ve onlarla oyun oynayabilir, yeni arkadaşlar edinip onlarla oyunlar oynayıp sohbet edebilirsin!');
+};
 
 
  
