137c5b8d2a6810dbd49201d00c2355211d73e28d
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-06-03 22:41:21 +0300

    Update dm.js

--- komutlar/dm.js
+++ komutlar/dm.js
@@ -1,15 +1,18 @@
+BOTA ÖZELDEN YAZILAN ŞEYLERİ GÖSTEREN KOD
 
-
-client.on("message", message => {
-    const dmchannel = client.channels.find("name", "s-dm");
+    client.on("message", message => {
+    const dmchannel = client.channels.find("name", "sapıklogu");
     if (message.channel.type === "dm") {
         if (message.author.id === client.user.id) return;
         dmchannel.sendMessage("", {embed: {
                 color: 3447003,
-                title: `Scary'e yazılan DM: ${message.author.tag}`,
+                title: `SİZİ SAPIKLAR SIZI DM: ${message.author.tag}`,
                 description: `${message.content}`
               }})
     }
     if (message.channel.bot) return;
 });
  
+
+
+
