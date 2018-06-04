917acbb83558aebabd8a2786752b9672238a3c27
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-06-04 07:21:03 +0300

    Create emoji.js

---
+++ komutlar/emoji.js
@@ -0,0 +1,6 @@
+client.on ('message', message => {
+if (message.content === prefix + "emojiler") {
+  const emojiList = message.guild.emojis.map(e=>e.toString()).join(" **|** ");
+  message.channel.send(emojiList);
+}
+})
