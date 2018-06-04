2284a4f32ec470eae4583538150e9f3398121a31
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-06-04 17:27:07 +0300

    Update bot - Kopya.js

--- bot - Kopya.js
+++ bot - Kopya.js
@@ -41,5 +41,6 @@ client.on('message', msg => {
    }
   }
 });
+client.on('guildMemberAdd', member => { let guild = member.guild; let joinRole = guild.roles.find('name', 'Üye'); // Burada girişte verilcek rolu seçelim. member.addRole(joinRole); // seçtiğimiz rolu verelim. const channel = member.guild.channels.find('name', 's-log'); // burda ise kanalı belirleyelim hangi kanala atsın ben mod-log dedim. if (!channel) return; const embed = new Discord.RichEmbed() .setColor('RANDOM') .setAuthor(member.user.username, member.user.avatarURL) .setThumbnail(member.user.avatarURL) .setTitle('📥 | Sunucuya katıldı!') .setTimestamp() channel.sendEmbed(embed); // belirlediğimiz kanala mesaj gönderelim.}); client.on('guildMemberRemove', member => { const channel = member.guild.channels.find('name', 's-log'); if (!channel) return; const embed = new Discord.RichEmbed() .setColor('RANDOM') .setAuthor(member.user.username, member.user.avatarURL) .setThumbnail(member.user.avatarURL) .setTitle('📤 | Sunucudan ayrıldı | Görüşmek üzere!') .setTimestamp() channel.sendEmbed(embed); });
 
 client.login(ayarlar.token);
