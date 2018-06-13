236e261558d02d2d2989b38123cce28caa17f9d1
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-06-13 12:31:31 +0300

    Update yrm.js

--- komutlar/yrm.js
+++ komutlar/yrm.js
@@ -1,17 +1,32 @@
-const prefix = "ayarlar.prefix";
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
- 
-	exports.run = ('message', msg => {
-    	if (msg.content === prefix + 'yardım') {
-	const embed = new Discord.RichEmbed()
-  	.setTitle("Bot Komutları")
-  	.setAuthor("Scary ∞ | Komutlar (s!) İle Başlar", "")
-  	.setColor("RANDOM")
-  	.setDescription('davet • Botun davet linkini atar.\nYeni Kodlar Gelicek\nbug • Bottaki bugu bildirmenizi sağlar.\nping • Botun pingini gösterir.\nsunucubilgi • Bu komutu hangi sunucuda kullanıysanız oranın bilgisini verir.\ntavsiye • Botun sahibine verdiğiniz tavsiyeyi gönderir.\n')
-  	.addField("Moderasyon Komutları", "ban • Belirttiğiniz kişiyi sunucudan banlar.\nkick • Belirttiğiniz kişiyi sunucudan atar.\nsustur • Belirttiğin kişiyi susturur.\ntemizle • Sohbeti belirttiğin sayı kadar siler.\nunban • Belirttiğin kişinin sunucudaki yasağını kaldırır.\noylama • Oylama Açarsınız.\nhastebin • Hastebine Kod Ekler.\n")
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message) => {
+  const embed = new Discord.RichEmbed()
+  
+  .setTitle("Bot Komutları")
+  .setAuthor("Scary | Yapımcı Yusuf", "")
+  .setColor("RANDOM")
+  .setDescription('s!davet • Botun davet linkini atar.\nYeni Kodlar Gelicek\ns!bug • Bottaki bugu bildirmenizi sağlar.\ns!ping • Botun pingini gösterir.\ns!sunucubilgi • Bu komutu hangi sunucuda kullanıysanız oranın bilgisini verir.\ns!tavsiye • Botun sahibine verdiğiniz tavsiyeyi gönderir.\n')
+  .setFooter("")
+  .setThumbnail("")
+  .setTimestamp()
+      .addField("Moderasyon Komutları", "ban • Belirttiğiniz kişiyi sunucudan banlar.\nkick • Belirttiğiniz kişiyi sunucudan atar.\nsustur • Belirttiğin kişiyi susturur.\ntemizle • Sohbeti belirttiğin sayı kadar siler.\nunban • Belirttiğin kişinin sunucudaki yasağını kaldırır.\noylama • Oylama Açarsınız.\nhastebin • Hastebine Kod Ekler.\n")
   	.addField("Eğlence Komutları", "gif•  Nice Gif.\nçayiç • Çay İçersiniz.\nbayrak • Şanlı Bayrağımız.\nparti • Parti Başlasın\npcaç • Bilgisayarı Açar.\nbanned • Dene Ve Gör.\n8ball • Bende Bilmiyorum.\nağla • Bot Ağlar.\natom • Atom Bombası.\ndeğişikahtapot • Çok Değişik.\nespri • Espri Atar.\nsigara • Sigara İçersiniz.\ntrollgifler • Troll.\n", true)
   	.addField(" Kullanıcı Komutları", "• Yeni Kodlar Gelicek\nkurucu • Sunucunun kurucusunu gösterir.\nkullanıcıbilgim • Bu komutu kullanan her kimse hakkında bilgi verir.\n", true)
   	message.channel.send({embed});
-	};
-	});	
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['bot bilgi', 'botbilgi', 'bb', 'botb', 'bbot', 'hakkında', 'bot hakkında', 'bothakkında'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'yardım',
+  description: 'Bot ile ilgili komut listesini verir.',
+  usage: 'yardım'
+};
