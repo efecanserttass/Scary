ff975801b1b4bda3fe6685fd26d844d805e9153c
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-06-17 14:58:31 +0300

    Update yrm.js

--- komutlar/yrm.js
+++ komutlar/yrm.js
@@ -13,8 +13,8 @@ exports.run = (client, message) => {
   .setThumbnail("")
   .setTimestamp()
       .addField("Moderasyon Komutları", "**ban • Belirttiğiniz kişiyi sunucudan banlar.\nkick • Belirttiğiniz kişiyi sunucudan atar.\nsustur • Belirttiğin kişiyi susturur.\ntemizle • Sohbeti belirttiğin sayı kadar siler.\nunban • Belirttiğin kişinin sunucudaki yasağını kaldırır.\noylama • Oylama Açarsınız.\nhastebin • Hastebine Kod Ekler.**\n")
-  	.addField("Eğlence Komutları", "**ngif•  Nice Gif.\nçayiç • Çay İçersiniz.\nbayrak • Şanlı Bayrağımız.\nparti • Parti Başlasın\npcaç • Bilgisayarı Açar.\nbanned • Dene Ve Gör.\n8ball • Bende Bilmiyorum.\nağla • Bot Ağlar.\natom • Atom Bombası.\ndeğişikahtapot • Çok Değişik.\nespri • Espri Atar.\nsigara • Sigara İçersiniz.\ntrollgifler • Troll.**\n", true)
-  	.addField(" Kullanıcı Komutları", "**• Yeni Kodlar Gelicek\nkurucu • Sunucunun kurucusunu gösterir.\nkullanıcıbilgim • Bu komutu kullanan her kimse hakkında bilgi verir.**\n", true)
+  	.addField("Eğlence Komutları", "**ngif•  Nice Gif.\nçayiç • Çay İçersiniz.\nbayrak • Şanlı Bayrağımız.\nparti • Parti Başlasın\npcaç • Bilgisayarı Açar.\nbanned • Dene Ve Gör.\n8ball • Bende Bilmiyorum.\nağla • Bot Ağlar.\natom • Atom Bombası.\ndeğişikahtapot • Çok Değişik.\nespri • Espri Atar.\nsigara • Sigara İçersiniz.\ntrollgifler • Troll.\nzekam • Zeka Bilgini Gösterir\nşanslı-sayım • Şanslı Sayınız\n", true)
+  	.addField(" Kullanıcı Komutları", "**bbilgi • Botun Bilgileri.\nsbilgi • Sunucu Bilgisi\nkurucu • Sunucunun kurucusunu gösterir.\nkullanıcıbilgim • Bu komutu kullanan her kimse hakkında bilgi verir.**\n", true)
     .addField("Müzik Komutları Beta [KAPALI]", "**çal • Müzik Başlar\ndur •Müzik Durur\ngeç •Müzik Geçer\nkuyruk • Müzik Kuyruğunu Gösterir**\n", true)
     .addField("Yapımcı Komutları", "**Burası Güvenlik Nedeni İle Gizlidir**\n", true)
     .addField("**__Yapımcı Yusuf#2022 Destekleyici Oxygen#7035__**")
@@ -26,7 +26,7 @@ exports.run = (client, message) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['y', 'botbilgi', 'bb', 'botb', 'bbot', 'hakkında', 'bot hakkında', 'bothakkında'],
+  aliases: ['y', 'help', 'ya', 'yrdm', 'komutlar', 'hakkında', 'bot hakkında', 'bothakkında'],
   permLevel: 0
 };
 
