0ae730c81a34fe0392ee8730c70b102a715cfa24
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-06-17 13:12:50 +0300

    Update ailemiz.js

--- komutlar/ailemiz.js
+++ komutlar/ailemiz.js
@@ -8,7 +8,7 @@ exports.run = (bot, message) => {
       embed.addField(`**${guild.name}** - ÜYE SAYISI : **${guild.memberCount}**`, guild.id);
       embed.setColor('#D97634')
       embed.setTitle('Ailemiz')
-      embed.setDescription(`Büyük bir ailedeyiz !. Ailemde **${bot.guilds.size}** kadar sunucu var !`)
+      embed.setDescription(`Buda Ailemiz Gene Koyduk !. Ailemizde **${bot.guilds.size}** kadar sunucu var !`)
     }
     message.channel.send({embed: embed});
   }
@@ -17,7 +17,7 @@ exports.run = (bot, message) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['s', 'konuş', 'sohbet', 'ask'],
+  aliases: ['sv', 'konuş', 'sohbet', 'ask'],
   permLevel: 0
 };
 
